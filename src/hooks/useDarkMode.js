798d8d34e257e6d131538b6a8ac9500f26a5ee21
Darrell-Conklin <dmconklin84@gmail.com>
2020-04-19 10:28:10 -0500

    removed false declaration from passed arguments to localStorage in useDarkMode since not decalring a value defaults to false

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,7 +2,7 @@ import { useLocalStorage } from './useLocalStorage';
 import { useEffect } from 'react';
 
 export const useDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode');
 
     useEffect(() => {
         darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
